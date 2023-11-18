835a26ca74f9f025592926ebe12d2c3a0642f1ca
KyleForan <kyleforan127@gmail.com>
2023-11-18 14:49:27 +0000

    new file:   .gitignore 	new file:   commands/clear.js 	new file:   commands/ping.js 	new file:   commands/user.js 	new file:   config.json 	new file:   deploy-commands.js 	new file:   index.js 	new file:   package.json

---
+++ commands/clear.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('clears messages in chat!')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+		.addIntegerOption(option => 
+			option.setName('amount')
+			.setDescription('Amount of messages to be cleared')
+			.setRequired(true)
+		)
+		.addUserOption(option => 
+			option.setName('target')
+			.setDescription('Specific users messages to delete')
+			.setRequired(false)
+		),
+	async execute(interaction) {
+
+		const { channel, options, client } = interaction
+
+		const target = options.getUser('target') 
+		let amount = options.getInteger('amount')
+
+		if (amount < 1) amount = 1
+		if (amount > 98) amount = 99
+		
+		let messages = await channel.messages.fetch({ limit: amount })
+		if (target) messages = await messages.filter(m => m.author.username == target.username)
+		
+		await channel.bulkDelete(messages)
+
+		await interaction.reply({
+			content: `Deleted ${messages.size} from ${target ? target : 'This channel'}!`,
+		});
+
+		setTimeout(async () => {
+			try {
+				const reply = await interaction.fetchReply()
+				await reply.delete()
+			} catch {
+				console.log('Message was deleted')
+			}
+		}, 2000)
+	},
+};
---
+++ deploy-commands.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js')
+const fs = require('node:fs')
+const path = require('node:path')
+const { clientId, guilds } = require('./config.json')
+
+require('dotenv').config()
+
+const commandspath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandspath).filter(file => file.endsWith('.js'))
+
+let commands = []
+
+for (file of commandFiles) {
+    const filepath = path.join(commandspath, file)
+    const command = require(filepath)
+
+    const { data, execute } = command
+
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON())
+    }
+    else {
+        console.warn(`${file} did not have both data and execute properties`)
+    }
+}
+
+const rest = new Discord.REST().setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Discord.Routes.applicationGuildCommands(clientId, guilds.test),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
